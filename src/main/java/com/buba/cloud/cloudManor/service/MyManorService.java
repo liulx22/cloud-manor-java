ddd0f22919f0e60baca8a283d6a9f573ae0b9913
qianquan <2897068188@qq.com>
2020-07-29 15:27:41 +0800

    邵垚飞-提交

--- src/main/java/com/buba/cloud/cloudManor/service/MyManorService.java
+++ src/main/java/com/buba/cloud/cloudManor/service/MyManorService.java
@@ -1,4 +1,19 @@
 package com.buba.cloud.cloudManor.service;
 
+import com.buba.cloud.cloudManor.pojo.OrderAgricultureVO;
+
+import java.util.List;
+
 public interface MyManorService {
+
+    //获取我的关注数量
+    int getAttentionNum(Integer userId);
+    //获取我的喜欢数量
+    int getMyLikeNum(Integer userId);
+    //获取我的评论数量
+    int getMyCommentNum(Integer userId);
+    //获取我的订单数量
+    int getMyOrderNum(Integer userId);
+    //获取我的庄园 当前农事和视频
+    List<OrderAgricultureVO> getOrderAgriculture(Integer userId);
 }
