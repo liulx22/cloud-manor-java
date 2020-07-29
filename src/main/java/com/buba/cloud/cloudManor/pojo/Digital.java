98299ba0d15a341e268bfef69bb2d972ceae1782
Administrator <18511539832@163.com>
2020-07-29 15:51:16 +0800

    1

--- src/main/java/com/buba/cloud/cloudManor/pojo/Digital.java
+++ src/main/java/com/buba/cloud/cloudManor/pojo/Digital.java
@@ -1,11 +1,14 @@
 package com.buba.cloud.cloudManor.pojo;
 
+import lombok.Data;
+
 /**
  * @Classname Digital
  * @Description 字典表（存储常量信息）
  * @Date 2020/7/23 15:19
  * @Created by liulx
  */
+@Data
 public class Digital {
     private Integer id;
     private String type;//类型   订单-order   图片类型-image
