b2f6feb942d290b2f78de5edc294d2af129e57db
zbw <865001374@qq.com>
2020-07-31 10:21:08 +0800

    张宝午提交

---
+++ src/main/java/com/buba/cloud/cloudManor/mapper/CultivatePersonCenterMapper.java
@@ -0,0 +1,29 @@
+package com.buba.cloud.cloudManor.mapper;
+
+import com.buba.cloud.cloudManor.pojo.Resource;
+import com.buba.cloud.cloudManor.pojo.User;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+/**
+ * @author admin
+ * @title: CultivatePersonCenterMapper
+ * @projectName cloud-manor-java
+ * @description: TODO
+ * @date 2020/7/290:16
+ */
+public interface CultivatePersonCenterMapper {
+    /*通过user id获取养护人信息*/
+    User pullMaintenanceInformation(@Param("userId") Integer userId);
+    /*通过user id查询对应资源数量*/
+    Integer pullTheNumberOfResources(@Param("userId") Integer userId);
+    /*通过user id查询对应的余额*/
+    Double pullMaintenanceBalance(@Param("userId")Integer userId);
+    /*通过user id查询发布的资源 (状态：已售出)*/
+    List<Resource> pullConservationResources(@Param("userId")Integer userId);
+    /*通过user id查询发布的资源 (状态：未售出)*/
+    List<Resource> pullConservatioNunsoldResources(@Param("userId")Integer userId);
+    /*添加农事表*/
+    boolean batchCommitResources(@Param("userId")Integer userId,@Param("content") String content, @Param("resourceId")int resourceId);
+}
