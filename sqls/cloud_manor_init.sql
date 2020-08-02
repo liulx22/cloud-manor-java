a3cd5cab7a6a9c640c9b1da0f8f50525284b70d4
liulx22 <liulx22@sina.com>
2020-08-02 12:37:10 +0800

    提交字典资源初始化数据

---
+++ sqls/cloud_manor_init.sql
@@ -0,0 +1,7 @@
+-- ----------------------------
+-- Records of digital
+-- ----------------------------
+INSERT INTO `digital` VALUES ('1', 'resource_main', '资源主图', 'resource');
+INSERT INTO `digital` VALUES ('2', 'resource_detail', '资源详情图', 'resource');
+INSERT INTO `digital` VALUES ('3', 'resource_vedio', '资源视频', 'resource');
+INSERT INTO `digital` VALUES ('4', 'resource_img', '资源图片', 'resource');
