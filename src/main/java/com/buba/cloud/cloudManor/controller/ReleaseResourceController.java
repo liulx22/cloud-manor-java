d3e8b8c08fdc5f0108ede0a96329474620ffc255
fan <f523_521@163.com>
2020-08-03 15:25:21 +0800

    范光辉-发布资源图片1

--- src/main/java/com/buba/cloud/cloudManor/controller/ReleaseResourceController.java
+++ src/main/java/com/buba/cloud/cloudManor/controller/ReleaseResourceController.java
@@ -1,7 +1,9 @@
 package com.buba.cloud.cloudManor.controller;
 
+import com.buba.cloud.cloudManor.pojo.Image;
 import com.buba.cloud.cloudManor.pojo.ResourceVO;
 import com.buba.cloud.cloudManor.service.ReleaseResourceService;
+import jdk.management.resource.ResourceId;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,8 +13,10 @@ import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.Iterator;
+import java.util.List;
 
 /**
  * @author Administrator
@@ -36,6 +40,7 @@ public class ReleaseResourceController {
     @RequestMapping("/image_video")
     @ResponseBody
     public boolean image_video(String type1,HttpServletRequest request) {
+        boolean b=false;
         //创建一个通用的多部分解析器
         CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver(request.getSession().getServletContext());
         //判断 request 是否有文件上传,即多部分请求
@@ -44,24 +49,49 @@ public class ReleaseResourceController {
             MultipartHttpServletRequest multiRequest = (MultipartHttpServletRequest) request;
             //取得request中的所有文件名
             Iterator<String> iter = multiRequest.getFileNames();
-            Date now = new Date();
+            int i=1;
             while (iter.hasNext()) {
+                Image image=new Image();
                 //取得上传文件
                 MultipartFile file = multiRequest.getFile(iter.next());
                 if (file != null) {
                     //取得当前上传文件的文件名称
                     String myFileName = file.getOriginalFilename();
-                    String resourceid=type1.substring(type1.indexOf("_")+1,type1.length());
-                    System.out.println(myFileName+"==="+type1+"==="+resourceid);
                     //如果名称不为“”,说明该文件存在，否则说明该文件不存在
-                   /* if (myFileName.trim() != "") {
-                        String type = myFileName.substring(0, myFileName.indexOf("-"));
-                        System.out.println(type);
-                    }*/
+                    if (myFileName.trim() != "") {
+                        //区分类型
+                        String typecode=type1.substring(0,type1.indexOf("_"));
+                        //资源id
+                        Integer resourceid= Integer.valueOf(type1.substring(type1.indexOf("_")+1,type1.indexOf("/")));
+                        //用户id
+                        String uid=type1.substring(type1.indexOf("/")+1,type1.indexOf("-"));
+                        //用户名字
+                        String username=type1.substring(type1.indexOf("-")+1,type1.length());
+                        System.out.println(myFileName+"==="+type1+"==="+resourceid+"=="+uid+"===="+username+"==-=="+i);
+                        //图片名称
+                        String imgName=username+"_"+uid+"_"+resourceid+"_"+i+".jpg";
+                        //图片code
+                        String typeCode=null;
+                        if (typecode.equals("1")){
+                            typeCode="resource_vedio";
+                        }else if (typecode.equals("2")){
+                            typeCode="resource_detail";
+                        }else if (typecode.equals("3")){
+                            typeCode="resource_main";
+                        }
+                        image.setImgDir("");
+                        image.setImgName(imgName);
+                        image.setTypeCode(typeCode);
+                        //图片上传
+                        if (resourceid!=null){
+                            b=releaseResourceService.resourceImgadd(image,resourceid);
+                        }
+                    }
+                   i++;
                 }
             }
         }
-        return true;
+        return b;
     }
     /*
      * @author fgh
--- src/main/java/com/buba/cloud/cloudManor/service/impl/ReleaseResourceServiceimpl.java
+++ src/main/java/com/buba/cloud/cloudManor/service/impl/ReleaseResourceServiceimpl.java
@@ -1,6 +1,7 @@
 package com.buba.cloud.cloudManor.service.impl;
 
 import com.buba.cloud.cloudManor.mapper.ReleaseResourceMapper;
+import com.buba.cloud.cloudManor.pojo.Image;
 import com.buba.cloud.cloudManor.pojo.ResourceVO;
 import com.buba.cloud.cloudManor.service.ReleaseResourceService;
 import org.springframework.stereotype.Service;
@@ -44,6 +45,34 @@ public class ReleaseResourceServiceimpl implements ReleaseResourceService {
                 b=releaseResourceMapper.resourceGroundadd(resourceVO);
             }else {
                 b=false;
+            }
+        }
+        return b;
+    }
+    /*
+     * @author fgh
+     * @description 图片视频详情上传
+     * @date 2020/8/2 13:32
+     * @param Image image, Integer resourceid
+     * @return boolean
+     */
+    @Override
+    public boolean resourceImgadd(Image image, Integer resourceid) {
+        boolean b=false;
+        //上传图片表
+        int i=releaseResourceMapper.resourceImgadd(image);
+        if (i>0){
+            //上传图片中间表
+            int typeCode=0;
+            if (image.getTypeCode().equals("resource_vedio")){
+                typeCode=3;
+            }else if (image.getTypeCode().equals("resource_detail")){
+                typeCode=2;
+            }else if (image.getTypeCode().equals("resource_main")){
+                typeCode=1;
+            }
+            if (typeCode!=0){
+                b=releaseResourceMapper.resourceImgRlationadd(image.getId(),resourceid,typeCode);
             }
         }
         return b;
