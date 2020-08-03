c35ee00d3c34d19db368992014563f7e06efcf9f
cr <15003479949@163.com>
2020-08-03 21:36:59 +0800

    崔燃--添加喜欢个取消喜欢--redis 01

--- src/main/java/com/buba/cloud/cloudManor/service/impl/UserLikeServiceImpl.java
+++ src/main/java/com/buba/cloud/cloudManor/service/impl/UserLikeServiceImpl.java
@@ -5,6 +5,7 @@ import com.buba.cloud.cloudManor.mapper.UserLikeMapper;
 import com.buba.cloud.cloudManor.pojo.Resource;
 import com.buba.cloud.cloudManor.pojo.UserLike;
 import com.buba.cloud.cloudManor.service.UserLikeService;
+import com.buba.cloud.cloudManor.utils.RedisUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,18 +15,43 @@ import java.util.List;
 public class UserLikeServiceImpl implements UserLikeService {
     @Autowired
     private UserLikeMapper userLikeMapper;
+    //调用redis
+    @Autowired
+    private  RedisUtils redisUtils;
     @Override
     public List<UserLike> selectUserLike(Integer userId) {
         return userLikeMapper.selectUserLike(userId);
     }
-
+//添加喜欢的作品
     @Override
     public int addUserLike(UserLike userLike) {
-        return userLikeMapper.addUserLike(userLike);
+        Integer rresourceId = userLike.getRresourceId();//获取作品id
+        String s = rresourceId.toString();//将int转为 String类型
+         redisUtils.hset("resourceId_number","resourceid_"+s,0);//先设置初始值
+        int i = userLikeMapper.addUserLike(userLike);//添加mysql操作
+        if (i!=0){//如果成功添加至mysql
+            Integer resourceId_number = (Integer)redisUtils.hget("resourceId_number", "resourceid_" + s);//获取redis中的点赞量
+            redisUtils.hset("resourceId_number","resourceid_"+s,resourceId_number+1);//让redis中的点赞量加一
+        }
+        return   (Integer)redisUtils.hget("resourceId_number", "resourceid_" + s);//返回redis的点赞量
     }
 
-    @Override
+
+
+//取消喜欢某个作品
+ @Override
     public int deleUserLike(UserLike userLike) {
-        return userLikeMapper.deleUserLike(userLike);
+     Integer rresourceId = userLike.getRresourceId();//获取作品id
+     String s = rresourceId.toString();//将int转为 String类型
+
+     int i = userLikeMapper.deleUserLike(userLike);//删除mysql操作
+     if (i!=0){//如果成功添加至mysql
+         Integer resourceId_number = (Integer)redisUtils.hget("resourceId_number", "resourceid_" + s);//获取redis中的点赞量
+         redisUtils.hset("resourceId_number","resourceid_"+s,resourceId_number-1);//让redis中的点赞量加一
+     }
+     return   (Integer)redisUtils.hget("resourceId_number", "resourceid_" + s);//返回redis的点赞量
+
+
+
     }
 }
