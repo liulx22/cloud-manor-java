89f0489841c2562ba5e921ef586c1be1233310d5
QianAlqq <3155579597@qq.com>
2020-08-06 17:25:09 +0800

    马东建提交1.5

--- src/main/java/com/buba/cloud/cloudManor/controller/NurseVerificationController.java
+++ src/main/java/com/buba/cloud/cloudManor/controller/NurseVerificationController.java
@@ -33,59 +33,63 @@ public class NurseVerificationController {
         String f2 = 1 + "_" + filename + "_03";
         //图片存放地址
         String a="https://lzj-picservice.oss-cn-beijing.aliyuncs.com";
-        List<File> files = new ArrayList<>();
-        //身份正后缀
-        String fileName = cardzheng.getOriginalFilename();
-        String suffix = fileName.substring(fileName.lastIndexOf("."), fileName.length());
-        //身份反后缀
-        String fileName1 = cardfan.getOriginalFilename();
-        String suffix1 = fileName1.substring(fileName1.lastIndexOf("."), fileName1.length());
-        //头像后缀
-        String fileName2 = shouchi.getOriginalFilename();
-        String suffix2 = fileName2.substring(fileName2.lastIndexOf("."), fileName2.length());
+        List<File> files = new ArrayList<File>();
+        if (cardzheng!=null| cardfan!=null| shouchi!= null) {
+            //身份正后缀
+            String fileName = cardzheng.getOriginalFilename();
+            String suffix = fileName.substring(fileName.lastIndexOf("."), fileName.length());
+            //身份反后缀
+            String fileName1 = cardfan.getOriginalFilename();
+            String suffix1 = fileName1.substring(fileName1.lastIndexOf("."), fileName1.length());
+            //头像后缀
+            String fileName2 = shouchi.getOriginalFilename();
+            String suffix2 = fileName2.substring(fileName2.lastIndexOf("."), fileName2.length());
 
 
-        //处理身份正
-        File file1=new File(f+suffix);
-        FileUtils.copyInputStreamToFile(cardzheng.getInputStream(), file1);
-        //身份反
-        File file2=new File(f1+suffix1);
-        FileUtils.copyInputStreamToFile(cardfan.getInputStream(), file2);
-        //头像
-        File file3=new File(f2+suffix2);
-        FileUtils.copyInputStreamToFile(shouchi.getInputStream(), file3);
+            //处理身份正
+            File file1 = new File(f + suffix);
+            FileUtils.copyInputStreamToFile(cardzheng.getInputStream(), file1);
+            //身份反
+            File file2 = new File(f1 + suffix1);
+            FileUtils.copyInputStreamToFile(cardfan.getInputStream(), file2);
+            //头像
+            File file3 = new File(f2 + suffix2);
+            FileUtils.copyInputStreamToFile(shouchi.getInputStream(), file3);
 
-        files.add(file1);
-        files.add(file2);
-        files.add(file3);
-        String keyName="CloudManor_Pic/IdentityInformation/";
-        if(fileName!=""|fileName1!=""|fileName2!=""){
-            OSSUtils.putUpload(files,keyName);
-        }else {
-            return false;
-        }
+            files.add(file1);
+            files.add(file2);
+            files.add(file3);
+            String keyName = "CloudManor_Pic/IdentityInformation/";
+            if (fileName != "" | fileName1 != "" | fileName2 != "") {
+                OSSUtils.putUpload(files, keyName);
+            } else {
+                return false;
+            }
 
-        Image image=new Image();
-        image.setImgName(f+suffix);
-        image.setImgDir(a+keyName);
-        image.setTypeCode("user_identity");
+            Image image = new Image();
+            image.setImgName(f + suffix);
+            image.setImgDir(a + keyName);
+            image.setTypeCode("user_identity");
 
-        //添加养护人信息
-        boolean b=nurseVerificationService.VerificationInsert(image,userId);
-        Image image1=new Image();
-        image1.setImgName(f+suffix);
-        image1.setImgDir(a+keyName);
-        image1.setTypeCode("user_identity");
-        boolean b1=nurseVerificationService.VerificationInsert(image1,userId);
+            //添加养护人信息
+            boolean b = nurseVerificationService.VerificationInsert(image, userId);
+            Image image1 = new Image();
+            image1.setImgName(f + suffix);
+            image1.setImgDir(a + keyName);
+            image1.setTypeCode("user_identity");
+            boolean b1 = nurseVerificationService.VerificationInsert(image1, userId);
 
-        Image image2=new Image();
-        image2.setImgName(f2+suffix);
-        image2.setImgDir(a+keyName);
-        image2.setTypeCode("user_identity");
-        boolean b2=nurseVerificationService.VerificationInsert(image2,userId);
-        if(b==true|b1==true|b2==true){
-            return true;
-        }else {
+            Image image2 = new Image();
+            image2.setImgName(f2 + suffix);
+            image2.setImgDir(a + keyName);
+            image2.setTypeCode("user_identity");
+            boolean b2 = nurseVerificationService.VerificationInsert(image2, userId);
+            if (b == true | b1 == true | b2 == true) {
+                return true;
+            } else {
+                return false;
+            }
+        }else{
             return false;
         }
 
